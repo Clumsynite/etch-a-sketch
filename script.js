bf823c16f2e0a654214d751f6972903b0096220f
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-06-14 17:39:04 +0530

    Added Reset Button
    1. Reset Button created
    2. Functions organised
    3. Need to make erase function and colors on hover

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 let container = document.getElementById('container');
+let resetBtn = document.getElementById('reset');
 
-
-function makegrid(grid){
-    
+function clearGrid() {
+    if(container.hasChildNodes()){
+        while(container.hasChildNodes()){
+            container.removeChild(container.lastChild);
+        }
+    }
+}
+function makegrid(grid){    
     container.style.gridTemplateColumns = `repeat(${grid}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${grid}, 1fr)`;
     for(let i=0;i<(grid*grid);i++){
         let cell = document.createElement('div');
         container.appendChild(cell).className = "gridCell";
     }
-    
 }
 
 function request(){
+    clearGrid();
     let gridSize = Number(prompt('Enter a grid size you want (Max size 64)'));
     if(gridSize>64){
         alert('Dude, I just told you the max size');
         makegrid(16);
     } else{
         makegrid(gridSize);
-    }}
+    }
+    paintBlack();
+}
+
 request();
 
-let gridCell = document.querySelectorAll('.gridCell');
-
-let cells = Array.from(gridCell);
-cells.forEach(elem => {
-    elem.addEventListener('mouseover', () => {
-        elem.style.backgroundColor = 'black';
-        elem.style.border = '1px solid white';
+function paintBlack(){
+    let gridCell = document.querySelectorAll('.gridCell');
+    let cells = Array.from(gridCell);
+    cells.forEach(elem => {
+        elem.addEventListener('mouseover', () => {
+            elem.classList.remove("gridCell");
+            elem.classList.add ("blackCell");
+        });
     });
+}
+
+resetBtn.addEventListener('click', () => {
+    request();
 });
